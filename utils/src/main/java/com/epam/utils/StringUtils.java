582366b00bfa8b3a3566fb010443657777922c3b
Ivan Artsiushyn <ivanart555@gmail.com>
2023-07-29 11:20:26 +0300

    Add string is a number check

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,7 +4,10 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        double number = NumberUtils.toDouble(str, Double.NaN);
-        return number > 0.0 && !Double.isNaN(number);
+        if (NumberUtils.isCreatable(str)) {
+            double number = NumberUtils.toDouble(str, Double.NaN);
+            return number > 0.0 && !Double.isNaN(number);
+        } else return false;
+
     }
 }
